2beb3922eef3baae37a7149a309e4ba08b64761f
Marcelo Gasparetti <mgasparetti86@gmail.com>
2021-02-10 18:51:43 -0300

    standard colors for charts

--- pages/chart.js
+++ pages/chart.js
@@ -1,6 +1,6 @@
 //Setting the Trasnactiosn variables:
-let expense = [1200,1400,1402,1410,1500,1410,1250,1800,1250,1560,1620,1622]
-let earn = [3000,3100,3200,3300,3400,3500,3600,3800,3900,4000,4100,4200]
+let expense = [1380,1400,1402,1410,1500,1410,1250,1800,1250,1560,1620,1622]
+let earn = [3080,3100,3200,3300,3400,3500,3600,3800,3900,4000,4100,4200]
 
 let ctx = document.getElementsByClassName("chart-expense")
 
@@ -26,8 +26,8 @@ let chartExpense = new Chart(ctx, {
       //pegar a soma de todos os valores de mês a Mês
       data: earn,
       borderWidth: 1,
-      borderColor: "#0066CC",
-      backgroundColor: "#0066CC",   
+      borderColor: "rgb(56, 180, 252)",
+      backgroundColor: "rgb(56, 180, 252)",   
     },
 
   ]
