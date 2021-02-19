0efc57a2501ce68b8df5434bbe2133ebd6198c34
Marcelo Gasparetti <mgasparetti86@gmail.com>
2021-02-19 15:59:36 -0300

    button filters and chart color

--- pages/chart.js
+++ pages/chart.js
@@ -1,39 +1,45 @@
 
 //Setting the Trasnactiosn variables:
-let expense = [1380,1400,1402,1410,1500,1410,1250,1800,1250,1560,1620,1622]
-let earn = [3080,3100,3200,3300,3400,3500,3600,3800,3900,4000,4100,4200]
+// let chartdata = document.querySelector("#data-input tbody")
 
-let ctx = document.getElementsByClassName("chart-construction")
 
+let income = [3080,3100,3200,3300,3400,3500,3600,3800,3900,4000,4100,4200]
+let expense = [3000,2400,2980,3200,2900,2410,2250,2230,2100,1560,1620,1622]
+
+const ctx = document.getElementsByClassName("chart-construction") 
 //Type, Data, and Options
 let chartExpense = new Chart(ctx, {
  
 //Type of Graph
   type: 'bar',
+
   //Data to be showed in the chart
   data: {
     labels: ["jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
     datasets: [
       {
-      label:"Gastos",
-      //pegar a soma de todos os valores de mês a Mês
+        label:"Income",
+        // Custom graphic of income transactions
+        data: income,
+        borderWidth: 1,
+        borderColor: "rgb(56, 180, 252)",
+        backgroundColor: "rgb(56, 180, 252)",
+        hoverBackgroundColor: "rgb(56, 180, 252, 0.25)",
+        hoverBorderColor: "rgb(185, 225, 235, 0.25)"
+      },
+      {
+      label:"Expense",
+      //Custom graphic of expense transactions
       data: expense,
       borderWidth: 1,
-      borderColor: "#FF0000",
-      backgroundColor: "#FF0000",   
+      borderColor: "rgb(255, 0, 0)",
+      backgroundColor: "rgb(255, 0, 0)",
+      hoverBackgroundColor: "rgb(255, 0, 0, 0.25)",
+      hoverBorderColor: "rgb(255, 0, 0, 0.25)"
     },
-      {
-      label:"Ganhos",
-      //pegar a soma de todos os valores de mês a Mês
-      data: earn,
-      borderWidth: 1,
-      borderColor: "rgb(56, 180, 252)",
-      backgroundColor: "rgb(56, 180, 252)",   
-    },
-
-  ]
+    ]
   },
-  //Somoe Option to display custom the Graphic
+  // Option it will be applied during the chart load
   option: {
     responsive: true,
     animation: {
